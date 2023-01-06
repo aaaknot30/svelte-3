702e76a157ef2ec882169bd9f29449e4bffb4ee6
Kyle Larson <113455306+aaaknot30@users.noreply.github.com>
2023-01-06 02:41:21 -0700

    Update ArtistCard.svelte

--- src/routes/ArtistCard.svelte
+++ src/routes/ArtistCard.svelte
@@ -11,7 +11,14 @@
   export let image_id;
   export let artist_display;
   export let place_of_origin;
+  export let alt_text;
   export let showLink;
+  export let credit_line;
+  export let date_display;
+  export let category_titles;
+  export let classification_titles;
+  export let medium_display;
+  export let index;
 
 
   function generateImageTag() {
@@ -32,14 +39,25 @@
 </script>
 
 <div>
+  <!-- {#if index !== undefined}<p>{index}</p>{/if} -->
+  {#if title !== undefined}<p>{title}</p>{/if}
   {#if showLink == "true"}
   <p><a href="/" on:click={generateArtistId}>{api_link}</a></p>
   {/if}
-  {#if title !== undefined}<p>{title}</p>{/if}
+
   {#if artist_title !== undefined}<p>{artist_title}</p>{/if}
   {#if image_id !== undefined}<p><a href="/" on:click={generateImageTag}>{image_id}</a></p>{/if}
   {#if artist_display !== undefined}<p>{artist_display}</p>{/if}
   {#if place_of_origin !== undefined}<p>{place_of_origin}</p>{/if}
+  {#if alt_text}<p>{alt_text}</p>{/if}
+  {#if credit_line}<p>{credit_line}</p>{/if}
+  {#if date_display}<p>{date_display}</p>{/if}
+  {#if category_titles}<p>{category_titles}</p>{/if}
+  {#if classification_titles}<p>{classification_titles}</p>{/if}
+  {#if medium_display}<p>{medium_display}</p>{/if}
+  
+  
+  
 </div>
 
 <style>
